20cd75471c86e2057a5b84cfea8c5f13abc757de
Akshath019 <akshathacharya2005@gmail.com>
2026-06-19 07:38:51 +0530

    day19

--- Codeforces - Codeforces Round 871 Div 4/B_Blank_Space/Main.java
+++ Codeforces - Codeforces Round 871 Div 4/B_Blank_Space/Main.java
@@ -4,9 +4,31 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+
+            int cnt = 0;
+            int max =  Integer.MIN_VALUE;
+
+
+            for(int i = 0 ; i < n ;i++){
+                int m = sc.nextInt();
+                if(m == 0){
+                    cnt++;
+                    max = Math.max(max , cnt);
+
+                }else{
+                    cnt = 0;
+                }
+            }
+            max = (max == Integer.MIN_VALUE) ? 0 : max;
+            System.out.println(max);
+        }
         
     }
 }
