3b4af9795dd90984f33650da13adfab3579bcb41
Akshath019 <akshathacharya2005@gmail.com>
2026-06-19 08:04:01 +0530

    day19

---
+++ Codeforces - Educational Codeforces Round 146 Rated for Div 2/A_Coins/Main.java
@@ -0,0 +1,26 @@
+//  A. Coins
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+
+            if (k % 2 != 0) {
+                System.out.println("YES");
+            } else if (n % 2 == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+        }
+}
